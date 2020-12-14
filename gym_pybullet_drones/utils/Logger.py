a7f081142eabaa1f1a8e3a96400ea4dc7a55d961
Luke <lukeschm@andrew.cmu.edu>
2020-12-14 04:20:50 -0500

    Fixed plotter

--- gym_pybullet_drones/utils/Logger.py
+++ gym_pybullet_drones/utils/Logger.py
@@ -27,16 +27,6 @@ class Logger(object):
         self.timestamps = np.zeros((num_drones,     duration_sec*self.LOGGING_FREQ_HZ))
         self.states     = np.zeros((num_drones, 16, duration_sec*self.LOGGING_FREQ_HZ))
         self.controls   = np.zeros((num_drones, 5,  duration_sec*self.LOGGING_FREQ_HZ))
-        # self.states = np.zeros((num_drones, 16, duration_sec*self.LOGGING_FREQ_HZ)) #### 16 states: pos_x, pos_y, pos_z,
-        #                                                                                                 # vel_x, vel_y, vel_z,
-        #                                                                                                 # roll, pitch, yaw,
-        #                                                                                                 # ang_vel_x, ang_vel_y, ang_vel_z,
-        #                                                                                                 # rpm0, rpm1, rpm2, rpm3
-        #### Note: this is not the same order nor length returned in obs["i"]["state"] by Aviary.step() ####
-        # self.controls = np.zeros((num_drones, 12, duration_sec*self.LOGGING_FREQ_HZ)) #### 12 control targets: pos_x, pos_y, pos_z,
-        #                                                                                                             # vel_x, vel_y, vel_z,
-        #                                                                                                             # roll, pitch, yaw,
-        #                                                                                                             # ang_vel_x, ang_vel_y, ang_vel_z
 
     ####################################################################################################
     #### Log entries for a single simulation step, of a single drone ###################################
@@ -48,7 +38,6 @@ class Logger(object):
     #### - control ((12,1) array)           drone's control target #####################################
     ####################################################################################################
     def log(self, drone: int, timestamp, state, control):
-        # if drone<0 or drone>=self.NUM_DRONES or timestamp<0 or len(state)!=20 or len(control)!=12: print("[ERROR] in Logger.log(), invalid data")
         current_counter = int(self.counters[drone])
         #### Add rows to the logging matrices every time a counter exceeds their shape #####################
         if current_counter>=self.timestamps.shape[1]:
@@ -61,8 +50,6 @@ class Logger(object):
         self.timestamps[drone,current_counter] = timestamp
         self.states[drone,:,current_counter]   = np.hstack([ state ])
         self.controls[drone,:,current_counter] = np.hstack([ control ])
-        # self.states[drone,:,current_counter] = np.hstack([ state[0:3], state[10:13], state[7:10], state[13:20] ])
-        # self.controls[drone,:,current_counter] = control
         self.counters[drone] = current_counter + 1
 
     ####################################################################################################
@@ -70,7 +57,6 @@ class Logger(object):
     ####################################################################################################
     def save(self):
         with open(os.path.dirname(os.path.abspath(__file__))+"/../../files/save-flight-"+datetime.now().strftime("%m.%d.%Y_%H.%M.%S")+".npy", 'wb') as out_file:
-            # np.save(out_file, self.timestamps); np.save(out_file, self.states);# np.save(out_file, self.controls)
             np.savetxt(out_file, (self.timestamps, self.states, self.controls), delimiter=',')
 
     ####################################################################################################
@@ -81,11 +67,9 @@ class Logger(object):
     ####################################################################################################
     def plot(self, pwm=False):
         #### Loop over colors and line styles ##############################################################
-        plt.rc('axes', prop_cycle=(cycler('color', ['r', 'g', 'b', 'y']) + cycler('linestyle', ['-', '--', ':', '-.'])))
-        # fig, axs = plt.subplots(8,2)        
+        plt.rc('axes', prop_cycle=(cycler('color', ['r', 'g', 'b', 'y']) + cycler('linestyle', ['-', '--', ':', '-.'])))  
         fig, axs = plt.subplots(6,2)
         t = np.arange(0, self.timestamps.shape[1]/self.LOGGING_FREQ_HZ, 1/self.LOGGING_FREQ_HZ)
-        # labels = ['X', 'Y', 'Z', 'VX', 'VY', 'VZ', 'R', 'P', 'Yaw', 'WR', 'WP', 'WY', 'PWM0', 'PWM1', 'PWM2', 'PWM3'] if pwm else ['X', 'Y', 'Z', 'VX', 'VY', 'VZ', 'R', 'P', 'Yaw', 'WR', 'WP', 'WY', 'RPM0', 'RPM1', 'RPM2', 'RPM3']
         labels = ['X', 'Y', 'Z', 'VX', 'VY', 'VZ', 'R', 'P', 'Yaw', 'WR', 'WP', 'WY'] if pwm else ['X', 'Y', 'Z', 'VX', 'VY', 'VZ', 'R', 'P', 'Yaw', 'WR', 'WP', 'WY']
         for i in range(len(labels)):
             for j in range(self.NUM_DRONES):
@@ -95,7 +79,6 @@ class Logger(object):
             axs[i%6,i//6].set_xlabel('time')
             axs[i%6,i//6].set_ylabel(labels[i])
             axs[i%6,i//6].grid(True)
-            # axs[i%8,i//8].legend(loc='upper right', frameon=True)
         fig.subplots_adjust(left=0.06, bottom=0.05, right=0.99, top=0.98, wspace=0.15, hspace=0.0)
         plt.show()
 
@@ -103,7 +86,7 @@ class Logger(object):
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
 
-        ax.plot(self.states[0,0,:], self.states[0,1,:], self.states[0,2,:]+0.4)
+        ax.plot(self.states[0,0,:], self.states[0,1,:], self.states[0,2,:])
 
         plt.xlim([-0.25, 0.25])
         plt.ylim([-0.25, 0.25])
